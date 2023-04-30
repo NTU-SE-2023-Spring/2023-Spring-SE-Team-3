ab43619f736555eaa0a95d67b6478873fc406191
featherchen <garychen0975321741@gmail.com>
2023-04-30 19:38:11 +0800

    add hw0-hw2

--- src/Person.java
+++
@@ -1,9 +0,0 @@
-public abstract class Person {
-    public String name;
-    public Person(String n){
-        name=n;
-    }
-    public abstract double getWeight();
-    public abstract double getHeight();
-    public abstract String getJob();
-}
