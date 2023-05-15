707c3f2973f3f7614adf0b27e234ce5f808e6ef0
featherchen <garychen0975321741@gmail.com>
2023-05-16 01:49:15 +0800

    fix(hw2): omit when java8

--- HW2_SpreadsheetApplication/test.sh
+++ HW2_SpreadsheetApplication/test.sh
@@ -2,6 +2,12 @@
 BASEDIR=$(dirname "$0")
 cd $BASEDIR
 
+JAVA_VER=$(java -version 2>&1 | head -1 | cut -d'"' -f2 | sed '/^1\./s///' | cut -d'.' -f1)
+if [ "$JAVA_VER" -eq "8" ]; then
+  echo "omit ${BASEDIR}"
+  exit 0
+fi
+
 echo "test ${BASEDIR}"
 
 echo "sample 1"
