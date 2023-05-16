3b9790485d0aa0ed15f700ea807559c406746a28
featherchen <garychen0975321741@gmail.com>
2023-05-16 12:48:46 +0800

    midterm: add ci

---
+++ Midterm/test.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+BASEDIR=$(dirname "$0")
+cd $BASEDIR
+
+echo "test ${BASEDIR}"
+
+echo "sample 1"
+java LibrarySystemn sample.in > output.out
+diff output.out sample.out
+if [ "$?" -ne "0" ]; then
+  echo "sample 1 WA"
+  exit 1
+fi
+echo "sample 1 AC"
+
+echo "sample 2"
+java LibrarySystem sample2.in > output2.out
+diff output2.out sample2.out
+if [ "$?" -ne "0" ]; then
+  echo "sample 2 WA"
+  exit 1
+fi
+echo "sample 2 AC"
