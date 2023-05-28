f12373faa692b623bc7e65fe37705d36f7c450c5
Zong Wei Hong <p458732@kimo.com>
2023-05-28 19:59:37 +0800

    fix: hw8

--- HW8_PrintOut/test.sh
+++ HW8_PrintOut/test.sh
@@ -13,20 +13,3 @@ if [ "$?" -ne "0" ]; then
 fi
 echo "sample 1 AC"
 
-echo "sample 2"
-java Main sampleInput2.in > output2.out
-diff output2.out sampleOutput2.out
-if [ "$?" -ne "0" ]; then
-  echo "sample 2 WA"
-  exit 1
-fi
-echo "sample 2 AC"
-
-echo "sample 3"
-java Main sampleInput3.in > output3.out
-diff output3.out sampleOutput3.out
-if [ "$?" -ne "0" ]; then
-  echo "sample 3 WA"
-  exit 1
-fi
-echo "sample 3 AC"
