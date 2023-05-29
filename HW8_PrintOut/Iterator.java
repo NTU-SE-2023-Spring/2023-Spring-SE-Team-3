ee415eb09d764b5ab7e127cf4081f41006c3eb68
Zong Wei Hong <p458732@kimo.com>
2023-05-29 10:24:08 +0800

    feat: update HW8

--- HW8_PrintOut/Iterator.java
+++ HW8_PrintOut/Iterator.java
@@ -9,20 +9,21 @@ interface Iterator {
 
 class List implements Iterator{
 
+    public String type = "List";
     public ArrayList<String> stringArrayList = new ArrayList<>();
     public int pos;
-    public String name;
+
     public int length;
 
     List(){}
-    List(String name){
-        this.name = name;
-        this.pos = 0;
-        this.length = 0;
-    }
+
 
     public boolean hasNext(){
+
         return !(pos>=length);
+    }
+    public void resetPos(){
+        this.pos = 0;
     }
     public Object next(){
         String res = stringArrayList.get(pos);
@@ -30,68 +31,73 @@ class List implements Iterator{
         return res;
     }
 
-    public void size(){
-        System.out.println("List do not have method size");
-    }
+
     public void getLength(){
         System.out.println(length);
     }
-    public void getNode(int index){
-        System.out.println("List do not have method getNode");
-    }
+
     public void addItem(String s){
         stringArrayList.add(s);
         length = stringArrayList.size();
     }
-    public void printOut(){
-        pos = 0;
-        while (hasNext()){
-            String s = (String) next();
-            System.out.println(s);
 
-
-        }
-
-    }
     public  void  get(int index){
         System.out.println(stringArrayList.get(index));
     }
 
 }
 
-class SkipList extends List{
+class SkipList implements Iterator{
 
-    SkipList(String name){
-        this.name = name;
+    public String type = "SkipList";
+
+    public ArrayList<SkipNode> stringArrayList = new ArrayList<>();
+    public int pos;
+
+    public int size;
+    SkipList(){
+
     }
-    @Override
+
     public void size(){
-        System.out.println(this.length);
+        System.out.println(this.size);
     }
 
-    @Override
-    public void getLength(){
-        System.out.println("SkipList can not access length");
+    public boolean hasNext(){
+
+        return !(pos>=size);
     }
-    @Override
+    public void resetPos(){
+        this.pos = 0;
+    }
+    public Object next(){
+        SkipNode res = stringArrayList.get(pos);
+        pos +=1;
+        return res;
+    }
+
+
+    public void addItem(String s){
+        stringArrayList.add(new SkipNode(s));
+        size = stringArrayList.size();
+    }
+
     public void getNode(int index){
-        System.out.println("SkipNode:" + stringArrayList.get(index));
+        System.out.println("SkipNode:" + stringArrayList.get(index).value);
     }
-    @Override
+
     public  void  get(int index){
         System.out.println("SkipList do not have method get");
     }
 
-    @Override
-    public void printOut(){
-        pos = 0;
-        while (hasNext()){
-            String s = (String) next();
-            System.out.println("SkipNode:" + s);
 
 
-        }
 
+};
+
+class SkipNode {
+    String value;
+    SkipNode(String v){
+        this.value = v;
     }
-
 }
--- HW8_PrintOut/Main.java
+++ HW8_PrintOut/Main.java
@@ -11,6 +11,8 @@ public class Main {
         BufferedReader reader = new BufferedReader(new FileReader(fakeDataFile));
         String line;
         Map<String, List> stringListMap = new HashMap<>();
+        Map<String, SkipList> stringSkipListMap = new HashMap<>();
+        Map<String, String>  nameMapType = new HashMap<>();
 
 
         while ((line = reader.readLine()) != null){
@@ -19,38 +21,82 @@ public class Main {
                 String name = line.split(" ")[1];
                 String type = line.split(" ")[2];
                 if (type.equals("List")){
-                    stringListMap.put(name, new List(name));
+                    stringListMap.put(name, new List());
+                    nameMapType.put(name, "List");
                 }else if (type.equals("SkipList")){
-                    stringListMap.put(name, new SkipList(name));
+                    stringSkipListMap.put(name, new SkipList());
+                    nameMapType.put(name, "SkipList");
                 }
 
 
             }else if (command.equals("Add")){
+
                 String name = line.split(" ")[1];
                 String val = line.split(" ")[2];
-                stringListMap.get(name).addItem(val);
+                if (nameMapType.get(name).equals("List")){
+                    stringListMap.get(name).addItem(val);
+                }else if (nameMapType.get(name).equals("SkipList")){
+                    stringSkipListMap.get(name).addItem(val);
+                }
+
 
             }else if (command.equals("Length")){
                 String name = line.split(" ")[1];
-                stringListMap.get(name).getLength();
+                if (nameMapType.get(name).equals("List")){
+                    stringListMap.get(name).getLength();
+                }else if (nameMapType.get(name).equals("SkipList")){
+                    System.out.println("SkipList can not access length");
+                }
+
 
             }else if (command.equals("Size")){
                 String name = line.split(" ")[1];
-                stringListMap.get(name).size();
+                if (nameMapType.get(name).equals("List")){
+                    System.out.println("List do not have method size");
+                }else if (nameMapType.get(name).equals("SkipList")){
+                    stringSkipListMap.get(name).size();
+                }
+
 
             }else if (command.equals("Get")){
                 String name = line.split(" ")[1];
                 int index = Integer.parseInt(line.split(" ")[2]);
-                stringListMap.get(name).get(index);
+                if (nameMapType.get(name).equals("List")){
+                    stringListMap.get(name).get(index);
+                }else if (nameMapType.get(name).equals("SkipList")){
+                    System.out.println("SkipList do not have method get");
+                }
+
+
 
             }else if (command.equals("GetNode")){
                 String name = line.split(" ")[1];
                 int index = Integer.parseInt(line.split(" ")[2]);
-                stringListMap.get(name).getNode(index);
+                if (nameMapType.get(name).equals("List")){
+                    System.out.println("List do not have method getNode");
+                }else if (nameMapType.get(name).equals("SkipList")){
+                    stringSkipListMap.get(name).getNode(index);
+                }
+
 
             }else if (command.equals("PrintOutList")){
                 String name = line.split(" ")[1];
-                stringListMap.get(name).printOut();
+                if (nameMapType.get(name).equals("List")){
+                    List l = stringListMap.get(name);
+                    l.resetPos();
+                    while(l.hasNext()){
+                        String s = (String )l.next();
+                        System.out.println(s);
+                    }
+
+                }else if (nameMapType.get(name).equals("SkipList")){
+                    SkipList l = stringSkipListMap.get(name);
+                    l.resetPos();
+                    while(l.hasNext()){
+                        String s = ((SkipNode)l.next()).value;
+                        System.out.println("SkipNode:" + s);
+                    }
+                }
 
             }
 
