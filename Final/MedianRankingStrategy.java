442cfc8d8d77d517fba479065b8327cc7770e7ff
TimWang <tim.wang@pranaq.com>
2023-06-06 14:24:24 +0800

    medianStrategy WIP

--- Final/MedianRankingStrategy.java
+++ Final/MedianRankingStrategy.java
@@ -1,4 +1,6 @@
+import java.util.Iterator;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Map;
 public class MedianRankingStrategy implements RankingStrategy{
     private static MedianRankingStrategy instance = new MedianRankingStrategy();
@@ -11,6 +13,38 @@ public class MedianRankingStrategy implements RankingStrategy{
     @Override
     public ArrayList<Double> rank(Score s,Map<String,Double> schoolStrategy) {
         // TODO Auto-generated method stub
-        return null;
+
+        ArrayList<Double> resultScoreList = new ArrayList<>();
+        Iterator<ArrayList<String>> itarrs = s.reviews.values().iterator();
+        ArrayList<String> arr = itarrs.next();
+        for (int i=0;i<arr.size();i++){
+            resultScoreList.add(0.0);
+        }
+        ArrayList<ArrayList<Double>> map = new ArrayList<>(null);
+        for (Student rst: s.reviews.keySet()){
+            ArrayList<Double> tmp = new ArrayList<>();
+            ArrayList<String> Lvlarr = s.reviews.get(rst);
+            for (int j=0;j<Lvlarr.size();j++){
+                String lvlString = Lvlarr.get(j);
+                Double dscore = schoolStrategy.get(lvlString);
+                assert dscore != null;
+                tmp.add(dscore);
+            }
+            map.add(tmp);
+
+        }
+        for(int i = 0; i < map.get(0).size(); i++){
+            ArrayList<Double> tmp = new ArrayList<>();
+            for(int j = 0; j < map.size(); j++){
+                tmp.add(map.get(j).get(i));
+            }
+            tmp.sort(Comparator.naturalOrder());
+            if(tmp.size() % 2 == 0){
+                resultScoreList.add(tmp.get(tmp.size()/2)+tmp.get(tmp.size()/2-1));
+            }else{
+                resultScoreList.add(tmp.get(tmp.size()/2));
+            }
+        }
+        return resultScoreList;
     }
 }
