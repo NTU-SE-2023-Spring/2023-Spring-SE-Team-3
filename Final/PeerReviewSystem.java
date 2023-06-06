0e54035b13c15b48109132e2cdcd41ed8217b4aa
featherchen <garychen0975321741@gmail.com>
2023-06-06 13:00:54 +0800

    PeerReviewSystem find

--- Final/PeerReviewSystem.java
+++ Final/PeerReviewSystem.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+
+import javax.script.ScriptContext;
 public class PeerReviewSystem {
     public ArrayList<Assignment> assignments = new ArrayList<>();
     public Map<String,Double> schoolStrategy = new HashMap<String,Double>();
@@ -68,17 +70,95 @@ public class PeerReviewSystem {
         
     }
     public void findStrength(String AID,String SID,String rs){
-        this.setRankingStrategy(rs);
-       
-
+        boolean setRsCorrect = this.setRankingStrategy(rs);
+        if (!setRsCorrect){
+            return;
+        }
+        Student st = students.get(SID);
+        if (st==null){
+            System.out.println("Error: findStrength: No such student");
+            return;
+        }
+        Assignment A = findAssignment(AID);
+        if (A==null){
+            System.out.println("Error: findStrength: assignment not found");
+            return;
+        }
+        Score s = A.getScore(st);
+        if (s==null){
+            System.out.println("Error: findStrength: student do not have this score");
+            return;
+        }
+        ArrayList<Double> score = this.rankingStrategy.rank(s,schoolStrategy);
+        // int strengthIndex = -1;
+        double strengthValue = -1;
+        for (int i=0;i<A.criterion.criteria.size();i++){
+            int result = Double.compare(score.get(i), strengthValue);
+            if(result > 0){
+                strengthValue = score.get(i);
+            }
+        }
+        ArrayList<String> strength = new ArrayList<>();
+        for (int i=0;i<A.criterion.criteria.size();i++){
+            int result = Double.compare(score.get(i), strengthValue);
+            if(result == 0){
+                strength.add(A.criterion.criteria.get(i));
+            }
+        }
+        String res = String.format("Assignment: %s, Student: %s, Strength:", AID, SID)
+        System.out.print(res);
+        for(int i=0;i<strength.size();i++){
+            System.out.print(" "+ strength.get(i));
+        }
+        System.out.print("\n");
     }
     public void findWeakness(String AID,String SID,String rs){
-
+        boolean setRsCorrect = this.setRankingStrategy(rs);
+        if (!setRsCorrect){
+            return;
+        }
+        Student st = students.get(SID);
+        if (st==null){
+            System.out.println("Error: findWeakness: No such student");
+            return;
+        }
+        Assignment A = findAssignment(AID);
+        if (A==null){
+            System.out.println("Error: findWeakness: assignment not found");
+            return;
+        }
+        Score s = A.getScore(st);
+        if (s==null){
+            System.out.println("Error: findWeakness: student do not have this score");
+            return;
+        }
+        ArrayList<Double> score = this.rankingStrategy.rank(s,schoolStrategy);
+        // int strengthIndex = -1;
+        double weaknessValue = -1;
+        for (int i=0;i<A.criterion.criteria.size();i++){
+            int result = Double.compare(score.get(i), weaknessValue);
+            if(result < 0){
+                weaknessValue = score.get(i);
+            }
+        }
+        ArrayList<String> weakness = new ArrayList<>();
+        for (int i=0;i<A.criterion.criteria.size();i++){
+            int result = Double.compare(score.get(i), weaknessValue);
+            if(result == 0){
+                weakness.add(A.criterion.criteria.get(i));
+            }
+        }
+        String res = String.format("Assignment: %s, Student: %s, Weakness:", AID, SID)
+        System.out.print(res);
+        for(int i=0;i<weakness.size();i++){
+            System.out.print(" "+ weakness.get(i));
+        }
+        System.out.print("\n");
     }
     
     private Assignment findAssignment(String AID){
         for (int i=0;i<assignments.size();i++){
-            if (assignments.get(i).name==AID){
+            if (assignments.get(i).ID==AID){
                 return assignments.get(i);
             }
         }
